8db52070fa288feb7d55fa6039df620765f2d127
Sehani Devindi <sehanidevindi@gmail.com>
2021-01-07 15:42:13 +0530

    initial commit

---
+++ CSharpDesignPatterns/com._01singleton/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns.com._01singleton
+{
+    class Logger
+	{
+        
+		private Logger() { }
+		private static Logger logger;
+
+		public static Logger GetInstance()
+		{
+			if (logger == null)
+			{
+				logger = new Logger();
+
+			}
+			return logger;
+		}
+	}
+}
---
+++ CSharpDesignPatterns/com._01singleton/TestLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns.com._01singleton
+{
+    class TestLogger
+    {
+		static void Main()
+		{
+			Console.WriteLine("---singleton---");
+			Logger obj1 = Logger.GetInstance();
+			Logger obj2 = Logger.GetInstance();
+
+			Console.WriteLine(obj1.GetHashCode());
+			Console.WriteLine(obj2.GetHashCode());
+
+
+			Console.WriteLine("---non singleton---");
+			Test obj3 = new Test();
+			Test obj4 = new Test();
+
+			Console.WriteLine(obj3.GetHashCode());
+			Console.WriteLine(obj4.GetHashCode());
+
+		}
+	}
+	class Test
+	{
+
+
+	}
+}
